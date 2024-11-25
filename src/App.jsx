4df48180c55d74b2c1a8169ea3f796fb67f102e5
Nidhish <nidhish.agarwal@kalvium.community>
2024-11-25 22:24:33 +0530

    made changes for mobile screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,31 @@
-import { useState } from 'react'
+import { useState , useEffect} from 'react'
 import styles from './App.module.css'
 import NavigationPanel from './components/NavigationPanel'
 import ChatSpace from './components/ChatSpace'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 786);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 786);
+    }
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+
+  }, [])
 
   return (
 
     <div className={styles.cover_main_container}>
       <div className={styles.main_container}>
-        <NavigationPanel />
+        {!isMobile && <NavigationPanel />}
+        
         <ChatSpace />
 
 
